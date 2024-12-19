b5d58b89dd8f5f4295f2fa6b119b6f3f1df85934
TanLocLouis <ltloc05lumia520@gmail.com>
2024-12-19 09:02:24 +0700

    Check whether all rating has already been rated

--- test.js
+++ test.js
@@ -1,37 +1,48 @@
-const surveyButtons = document.querySelectorAll("a[onclick*='loadSurvey']");
-surveyButtons.forEach((button, index) => {
-    button.click();
-    function autofill(stars) {
-        const tableCells = document.querySelectorAll('td');
+function Rating() {
+    const numberOfPages = 8;
 
-        tableCells.forEach((cell) => {
-            // Find all <input> elements inside the current <td>
-            const inputs = cell.querySelectorAll('input[type="radio"]');
-            if (inputs.length > 0) {
+    const surveyButtons = document.querySelectorAll("a[onclick*='loadSurvey']");
+    if (surveyButtons.length === 7) {
+        alert("Cac mon hoc da duoc danh gia :))");
+        return;
+    }
+    surveyButtons.forEach((button, index) => {
+        button.click();
+        function autofill(stars) {
+            const tableCells = document.querySelectorAll('td');
 
-            // Select the last <input> element and check it
-            const lastInput = inputs[stars - 1];
-            lastInput.checked = true; // Auto-check the last radio button
+            tableCells.forEach((cell) => {
+                // Find all <input> elements inside the current <td>
+                const inputs = cell.querySelectorAll('input[type="radio"]');
+                if (inputs.length > 0) {
+
+                // Select the last <input> element and check it
+                const lastInput = inputs[stars - 1];
+                lastInput.checked = true; // Auto-check the last radio button
+            }
+            })
         }
-        })
-    }
+        
+        // Set the number of stars
+        let stars = 5;
+        stars = prompt("Ban muon danh gia giang vien may sao?: ");
+        autofill(stars)
 
-    let stars = 5;
-    stars = prompt("Ban muon danh gia giang vien may sao?: ");
-    autofill(stars)
+        // Next to 8 pages
+        for (let i = 0; i < numberOfPages; i++) {
+            nextSection()
+        }
 
-    // Next to 8 pages
-    for (let i = 0; i < 8; i++) {
-        nextSection()
-    }
+        // Submit the form
+        const saveButton = document.getElementById("btnSave");
+        saveButton.click();
 
-    // Submit the form
-    const saveButton = document.getElementById("btnSave");
-    saveButton.click();
+        // Back to main page
+        showSurveyForm(false);
+        loadDefault(loai);
+        loadDSDanhGia();
+    });
+}
 
-    // Back to main page
-    showSurveyForm(false);
-    loadDefault(loai);
-    loadDSDanhGia();
-});
-
+// Call main funtion
+Rating()
